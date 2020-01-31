8f8ed0d28942edc4738a383df6d45e62e09dcb02
Charlie03 <ceciampa@gmail.com>
2020-01-31 10:46:52 -0500

    Working on Autonomous.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ReadingsBotAutonomous.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 The Newman School Robotics
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+/**
+ * Created by Galvin on 2020-01-11
+ */
+@Autonomous(name = "Readings (Gives readings from sensors/encoders)", group = "Autonomous")
+public class ReadingsBotAutonomous extends OpMode {
+    @Override
+    public void init() {
+    }
+    @Override
+    public void loop() {
+    }
+}
